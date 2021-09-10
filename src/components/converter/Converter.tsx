eb7fb401e276c4a8b8d93ff7269ea20e4c968ada
Gabriel Donas <osradecky@gmail.com>
2021-09-10 19:44:30 +0100

    update css class names

--- src/components/converter/Converter.tsx
+++ src/components/converter/Converter.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { useState } from "react";
 import binaryToDecimal from "../../helpers/binaryToDecimal";
 import checkBinary from "../../helpers/checkBinary";
@@ -5,22 +6,27 @@ import styles from "./converter.module.css";
 
 function Converter() {
   const [output, setOutput] = useState("0");
+  const errorMessage = "Wrong input, please enter either 0 or 1";
+
+  const classes = classNames(
+    { [styles.result]: output !== errorMessage },
+    { [styles.wrongInput]: output === errorMessage }
+  );
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (checkBinary(e.target.value)) {
       setOutput(binaryToDecimal(e.target.value));
     } else {
-      setOutput("Wrong Input, please enter either 0 or 1");
+      setOutput(errorMessage);
     }
   }
 
   return (
     <div className={styles.container}>
       <input type="number" name="binary" onChange={handleChange} />
-      <div className={styles.result}>{output}</div>
+      <div className={classes}>{output}</div>
     </div>
   );
 }
-//change class when not binary
 
 export default Converter;
