12d2328490e2cc7d9bb91520ea4e58fd4ebfd884
Gabriel Donas <osradecky@gmail.com>
2021-09-11 22:41:44 +0100

    refactor and improve the converter tests

--- src/components/converter/Converter.test.tsx
+++ src/components/converter/Converter.test.tsx
@@ -11,15 +11,33 @@ describe("The Converter component", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it("Should give a warning with the input is not a binary", () => {
-    wrapper.find("input").simulate("change", { target: { value: "123" } });
-    expect(wrapper.text()).toBe("Wrong input, please enter either 0 or 1");
+  describe("Incorrect Input, non binary", () => {
+    beforeAll(() => {
+      wrapper.find("input").simulate("change", { target: { value: "123" } });
+    });
+
+    it("Should show a warning if the input is not a binary", () => {
+      expect(wrapper.text()).toBe("Wrong input, please enter either 0 or 1");
+    });
+
+    it("Should render the component with the .wrongInput class if input is not binary", () => {
+      expect(wrapper.find("div.wrongInput").hasClass("wrongInput")).toBe(true);
+    });
   });
 
-  it("Should return a decimal number if the input is a binary", () => {
-    wrapper
-      .find("input")
-      .simulate("change", { target: { value: "111110000111" } });
-    expect(wrapper.text()).toBe("3975");
+  describe("Correct Input, binary", () => {
+    beforeAll(() => {
+      wrapper
+        .find("input")
+        .simulate("change", { target: { value: "111110000111" } });
+    });
+
+    it("Should return a decimal number if the input is a binary", () => {
+      expect(wrapper.text()).toBe("3975");
+    });
+
+    it("Should render the component with the .result class if input is binary", () => {
+      expect(wrapper.find("div.result").hasClass("result")).toBe(true);
+    });
   });
 });
